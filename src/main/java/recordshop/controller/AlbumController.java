e15fb9bfec9af32a19b90779a427cd0b1ba829ea
yee0802 <NovusAa12@proton.me>
2024-12-06 11:42:17 +0000

    feat: add getAlbumById to AlbumController

--- src/main/java/recordshop/controller/AlbumController.java
+++ src/main/java/recordshop/controller/AlbumController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import recordshop.model.Album;
@@ -23,5 +24,10 @@ public class AlbumController {
         List<Album> albums = albumService.getAllAlbums();
 
         return new ResponseEntity<>(albums, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Album> getAlbumById(@PathVariable Long id) {
+        return new ResponseEntity<>(albumService.getAlbumById(id), HttpStatus.OK);
     }
 }
