1a69a6ef7fc6ea369763000fe68d05591e102ff0
Kye <NovusAa12@proton.me>
2024-12-16 10:43:43 +0000

    feat: add unique constraint to name in Artist model

--- src/main/java/recordshop/model/Artist.java
+++ src/main/java/recordshop/model/Artist.java
@@ -22,7 +22,7 @@ public class Artist {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @JsonManagedReference
