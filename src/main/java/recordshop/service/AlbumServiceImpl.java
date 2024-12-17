1da72edb90a6e88ae67817748e80c2941e5926f2
Kye <NovusAa12@proton.me>
2024-12-17 17:13:15 +0000

    refactor: update AlbumServiceImpl to accommodate DTOs

--- src/main/java/recordshop/service/AlbumServiceImpl.java
+++ src/main/java/recordshop/service/AlbumServiceImpl.java
@@ -5,6 +5,8 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import recordshop.dto.AlbumDTO;
+import recordshop.dto.ArtistDTO;
 import recordshop.exception.ItemNotFoundException;
 import recordshop.exception.MissingFieldException;
 import recordshop.model.Album;
@@ -25,70 +27,74 @@ public class AlbumServiceImpl implements AlbumService {
     ArtistRepository artistRepository;
 
     @Override
-    public List<Album> getAllAlbums() {
+    public List<AlbumDTO> getAllAlbums() {
         List<Album> albums = new ArrayList<>();
 
         albumRepository.findAll().forEach(albums::add);
 
-        return albums;
+        return albums.stream()
+                .map(this::mapToDTO)
+                .toList();
     }
 
     @Override
     @Cacheable(cacheNames = "albums", key = "#id")
-    public Album getAlbumById(Long id) {
-        return albumRepository.findById(id)
+    public AlbumDTO getAlbumById(Long id) {
+        Album foundAlbum = albumRepository.findById(id)
                 .orElseThrow(() -> new ItemNotFoundException(String.format("Album with id '%s' could not be found", id)));
+
+        return mapToDTO(foundAlbum);
     }
 
     @Override
-    public Album addAlbum(Album album) {
-        boolean hasValidFields = requestBodyHasValidFields(album);
+    public AlbumDTO addAlbum(AlbumDTO albumDTO) {
+        boolean hasValidFields = requestBodyHasValidFields(albumDTO);
 
         if (!hasValidFields) {
             throw new MissingFieldException("Missing field(s) in request body");
         }
 
-        Artist artist = artistRepository.findByName(album.getArtist().getName());
+        Artist artist = artistRepository.findByName(albumDTO.getArtist().getName());
 
         if (artist == null) {
-            artist = artistRepository.save(album.getArtist());
+            artist = artistRepository.save(mapToEntity(albumDTO.getArtist()));
         }
 
-        Album newAlbum = new Album();
-        newAlbum.setName(album.getName());
-        newAlbum.setArtist(artist);
-        newAlbum.setGenre(album.getGenre());
-        newAlbum.setReleaseYear(album.getReleaseYear());
-        newAlbum.setStockQuantity(album.getStockQuantity());
+        Album album = mapToEntity(albumDTO);
+        album.setArtist(artist);
 
-        return albumRepository.save(album);
+        Album savedAlbum = albumRepository.save(album);
+
+        return mapToDTO(savedAlbum);
     }
 
     @Override
     @CachePut(cacheNames = "albums", key = "#id")
-    public Album updateAlbumById(Long id, Album album) {
+    public AlbumDTO updateAlbumById(Long id, AlbumDTO albumDTO) {
         Album foundAlbum = albumRepository.findById(id)
                 .orElseThrow(() -> new ItemNotFoundException(String.format("Album with id '%s' could not be found", id)));
 
-        boolean hasValidFields = requestBodyHasValidFields(album);
+        boolean hasValidFields = requestBodyHasValidFields(albumDTO);
 
         if (!hasValidFields) {
             throw new MissingFieldException("Missing field(s) in request body");
         }
 
-        Artist artist = artistRepository.findByName(album.getArtist().getName());
+        Artist artist = artistRepository.findByName(albumDTO.getArtist().getName());
 
         if (artist == null) {
             throw new ItemNotFoundException(String.format("Artist with id '%s' could not be found", id));
         }
 
-        foundAlbum.setName(album.getName());
+        foundAlbum.setName(albumDTO.getName());
         foundAlbum.setArtist(artist);
-        foundAlbum.setGenre(album.getGenre());
-        foundAlbum.setReleaseYear(album.getReleaseYear());
-        foundAlbum.setStockQuantity(album.getStockQuantity());
+        foundAlbum.setGenre(albumDTO.getGenre());
+        foundAlbum.setReleaseYear(albumDTO.getReleaseYear());
+        foundAlbum.setStockQuantity(albumDTO.getStockQuantity());
 
-        return albumRepository.save(foundAlbum);
+        Album updatedAlbum = albumRepository.save(foundAlbum);
+
+        return mapToDTO(updatedAlbum);
     }
 
     @Override
@@ -100,12 +106,66 @@ public class AlbumServiceImpl implements AlbumService {
         albumRepository.deleteById(id);
     }
 
-    private boolean requestBodyHasValidFields(Album album) {
-        return album.getName() != null
-                && album.getGenre() != null
-                && album.getReleaseYear() != null
-                && album.getStockQuantity() != null
-                && album.getArtist() != null
-                && album.getArtist().getName() != null;
+    private boolean requestBodyHasValidFields(AlbumDTO albumDTO) {
+        return albumDTO.getName() != null
+                && albumDTO.getGenre() != null
+                && albumDTO.getReleaseYear() != null
+                && albumDTO.getStockQuantity() != null
+                && albumDTO.getArtist() != null
+                && albumDTO.getArtist().getName() != null;
+    }
+
+    AlbumDTO mapToDTO(Album album){
+        AlbumDTO albumDTO = new AlbumDTO();
+
+        albumDTO.setId(album.getId());
+        albumDTO.setName(album.getName());
+        albumDTO.setGenre(album.getGenre());
+        albumDTO.setArtist(mapToDTO(album.getArtist()));
+        albumDTO.setCoverArtUrl(album.getCoverArtUrl());
+        albumDTO.setReleaseYear(album.getReleaseYear());
+        albumDTO.setStockQuantity(album.getStockQuantity());
+        albumDTO.setCreatedAt(album.getCreatedAt());
+        albumDTO.setModifiedAt(album.getModifiedAt());
+
+        return albumDTO;
+    }
+
+    ArtistDTO mapToDTO(Artist artist){
+        ArtistDTO artistDTO = new ArtistDTO();
+
+        artistDTO.setId(artist.getId());
+        artistDTO.setName(artist.getName());
+        artistDTO.setCreatedAt(artist.getCreatedAt());
+        artistDTO.setModifiedAt(artist.getModifiedAt());
+
+        return  artistDTO;
+    }
+
+    Album mapToEntity(AlbumDTO albumDTO) {
+        Album album = new Album();
+
+        album.setId(albumDTO.getId());
+        album.setName(albumDTO.getName());
+        album.setArtist(mapToEntity(albumDTO.getArtist()));
+        album.setGenre(albumDTO.getGenre());
+        album.setCoverArtUrl(albumDTO.getCoverArtUrl());
+        album.setReleaseYear(albumDTO.getReleaseYear());
+        album.setStockQuantity(albumDTO.getStockQuantity());
+        album.setCreatedAt(albumDTO.getCreatedAt());
+        album.setModifiedAt(albumDTO.getModifiedAt());
+
+        return album;
+    }
+
+    Artist mapToEntity(ArtistDTO artistDTO) {
+        Artist artist = new Artist();
+
+        artist.setId(artistDTO.getId());
+        artist.setName(artistDTO.getName());
+        artist.setCreatedAt(artistDTO.getCreatedAt());
+        artist.setModifiedAt(artistDTO.getModifiedAt());
+
+        return artist;
     }
 }
