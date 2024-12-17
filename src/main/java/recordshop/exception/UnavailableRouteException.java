0c1186671ef3427e9b8007e8d7a3ef4254cdaac7
Kye <NovusAa12@proton.me>
2024-12-17 14:34:18 +0000

    feat: add UnavailableRouteException

---
+++ src/main/java/recordshop/exception/UnavailableRouteException.java
@@ -0,0 +1,7 @@
+package recordshop.exception;
+
+public class UnavailableRouteException extends RuntimeException {
+    public UnavailableRouteException(String message) {
+        super(message);
+    }
+}
