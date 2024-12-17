5802635cf8ec9c0adb10577d9d9186a447d9dd34
Kye <NovusAa12@proton.me>
2024-12-17 19:01:17 +0000

    refactor: remove createErrorResponse from GlobalExceptionHandler

--- src/main/java/recordshop/exception/GlobalExceptionHandler.java
+++ src/main/java/recordshop/exception/GlobalExceptionHandler.java
@@ -5,33 +5,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.LocalDateTime;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<Object> handleItemNotFoundException(ItemNotFoundException e) {
-        return new ResponseEntity<>(createErrorResponse(404, e.getMessage()), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse(404, e.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler
     public ResponseEntity<Object> handleMissingFieldException(MissingFieldException e) {
-        return new ResponseEntity<>(createErrorResponse(400, e.getMessage()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse(400, e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
     public ResponseEntity<Object> handleUnavailableRouteException(UnavailableRouteException e) {
-        return new ResponseEntity<>(createErrorResponse(404, e.getMessage()), HttpStatus.NOT_FOUND);
-    }
-
-    private ErrorResponse createErrorResponse(int statusCode, String message) {
-        ErrorResponse responseObj = new ErrorResponse();
-
-        responseObj.setStatus(statusCode);
-        responseObj.setMessage(message);
-        responseObj.setTimestamp(LocalDateTime.now());
-
-        return responseObj;
+        return new ResponseEntity<>(new ErrorResponse(404, e.getMessage()), HttpStatus.NOT_FOUND);
     }
 }
