0b875fd22deebf384b638cb4cc6f4ca4932b9dba
Kye <NovusAa12@proton.me>
2024-12-17 14:32:53 +0000

    refactor: update GlobalExceptionHandler to utilise ErrorResponse

--- src/main/java/recordshop/exception/GlobalExceptionHandler.java
+++ src/main/java/recordshop/exception/GlobalExceptionHandler.java
@@ -5,16 +5,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<Object> handleItemNotFoundException(ItemNotFoundException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(createErrorResponse(404, e.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler
     public ResponseEntity<Object> handleMissingFieldException(MissingFieldException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(createErrorResponse(400, e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    private ErrorResponse createErrorResponse(int statusCode, String message) {
+        ErrorResponse responseObj = new ErrorResponse();
+
+        responseObj.setStatus(statusCode);
+        responseObj.setMessage(message);
+        responseObj.setTimestamp(LocalDateTime.now());
+
+        return responseObj;
     }
 }
