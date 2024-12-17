d5d122603925b7995edfafe49df615f1184305ea
Kye <NovusAa12@proton.me>
2024-12-17 14:36:49 +0000

    refactor: update handleUnavailableRoutes to throw Exception

--- src/main/java/recordshop/controller/FallbackController.java
+++ src/main/java/recordshop/controller/FallbackController.java
@@ -1,15 +1,15 @@
 package recordshop.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import recordshop.exception.UnavailableRouteException;
 
 @RestController
 public class FallbackController {
 
     @RequestMapping("/**")
     public ResponseEntity<String> handleUnavailableRoutes() {
-        return new ResponseEntity<>("The requested endpoint was not found", HttpStatus.NOT_FOUND);
+        throw new UnavailableRouteException("The requested endpoint was not found");
     }
 }
